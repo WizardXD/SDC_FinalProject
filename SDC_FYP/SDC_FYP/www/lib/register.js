ce2186fb71f7c1bce3c1b667e154acca7e717df1
TanYongAN <33213065+TanYongAn@users.noreply.github.com>
2018-10-07 19:51:58 +0800

    ...

--- SDC_FYP/SDC_FYP/www/lib/register.js
+++ SDC_FYP/SDC_FYP/www/lib/register.js
@@ -1,46 +1,56 @@
 ﻿(function () {
     "use strict";
     $(document).ready(function () {
+        var counter = 2;
 
-        // add/remove input field
-        //here first get the contents of the div with name class copy-fields and add it to after "after-add-more" div class.
-        $(".add-more").click(function () {                  
-            var html = $(".copy-fields").html();            
-            $(".membernames").after(html);
+            $("#addMemberbtn").click(function () {
+                if (counter > 6) {                                      //maximum members allowed = 6, will adjust based on what the sponsor requrested.
+                    alert("The maximum number of members is 6");        //message that will be alerted if members > 6
+                    return false;
+                }
+                
+            if (counter >= 3) {
+                var newGroupMember = $(document.createElement('div'))    //create a new div element
+                    .attr("id", 'GroupMember' + counter);                //.attr(attribute, value)
+                    newGroupMember.after().html('<p><ons-input name="membernames' + counter + '" id="membernames' + counter + '" value="" placeholder="Member ' + counter + ' Name"></ons-input><p/>');   //.after --> insert contetnt after....
+                    newGroupMember.appendTo("#GroupMembersName");  //.apendTo("#")  <-- insert every element into GroupMembersName
+                    counter++;
+                }
+
+            if (counter == 2) {                                          // only the second text input field will have the 'remove members'/ minus sign
+                var newGroupMember = $(document.createElement('div'))    //create a new div element
+                    .attr("id", 'GroupMember' + counter);                //.attr(attribute, value)
+                newGroupMember.after().html('<p><ons-input name="membernames' + counter + '" id="membernames' + counter + '" value="" placeholder="Member ' + counter + ' Name" style="width:166px"></ons-input>' +
+                                            '&nbsp;<input type="button" value="- " id="removeMemberbtn"><p/>');
+                newGroupMember.appendTo("#GroupMembersName");
+                counter++;
+
+                $("#removeMemberbtn").click(function () {
+                    if (counter == 2) {
+                        //alert("No more textbox to remove");
+                        return false;
+                    }
+                    counter--;
+                    $("#GroupMember" + counter).remove();
+                });
+                
+            }
+
         });
-        //here it will remove the current value of the remove button which has been pressed
-        $("body").on("click", ".remove", function () {
-            $(this).parents(".control-group").remove();
-        });
-        
+
+       
+        //can delete afterwards
+        $("#getButtonValue").click(function () {
+            var msg = '';
+            for (i = 1; i < counter; i++) {
+                msg = "\n Member " + i + " : " + $('#membernames' + i).val();
+                alert(msg);
+            }
+        }); 
+
         $("#registerbtn").bind("click", function () {
-                registerUser();
+                alert("Hi");
         });
     });
-
-    function registerUser() {
-    //var groupname;
-    //var username;
-    //var password;
-    //var password2;
-    //var school;
-    var name1;
-    var name2;
-
-    //groupname = $("#newgroupname").val();
-    //username = $("#newusername").val();
-    //password = $("#newpassword").val();
-    //password2 = $("#newpassword2").val();
-    //school = $("#school").val();
-    name1 = $("#name1").val();
-    name2 = $("#name2").val();
-
-    //alert(groupname);
-    //alert(username);
-    //alert(password);
-    //alert(school);
-    alert(name1);
-    alert(name2);
-    }
 })();
 
