9d2acbd04e484dd59a04afcc708bb166f0187aef
WizardXD <royston123456@gmail.com>
2018-10-08 16:38:58 +0800

    At facilitator page (Doing account_control)

--- SDC_FYP/SDC_FYP/www/lib/game_verifycode.js
+++ SDC_FYP/SDC_FYP/www/lib/game_verifycode.js
@@ -33,36 +33,50 @@ var hidePopover = function () {
 };
 
 var test = function () {
-    console.log("asda");
+    gameStartVerfication();
 };
 
 
 
 
-function startgame() {
-   var url = serverURL() + "/game.php"; //have an issue 
+function gameStartVerfication() {
+   var url = serverURL() + "/gameverifycode.php"; //have an issue 
    var gamecode = $("#txtGameCode").val();
 
-
-
+  
+    
     //JSONObject
     JSONObject = {
-        "gamecode": game, //Key:value
+        "gamecode": gamecode //Key:value
         
     };
 
-    //ajax to call
+    //ajax call
     $.ajax({
         url: url, //A string containing the URL to which the request is sent.
         type: 'GET',
         data: JSONObject, //Data to be sent to the server. It is converted to a query string, if not already a string. It's appended to the url for GET-requests.
         dataType: 'json', //Evaluates the response as JSON and return a JS object
         contentType: "application/json; charset=utf-8",
-        success: function (data) { //function to be called if the request succeeds //this function will be called
+        success: function (verifygameid) { //function to be called if the request succeeds //this function will be called
+            if (verifygameid.result == 1) {
+                window.location = "../game.html";
+            }
 
-
+            else {
+                window.location = "#";
+            }
+            
 
 
         }
+
+        
     });
+
+    
+   
 }
+
+
+
