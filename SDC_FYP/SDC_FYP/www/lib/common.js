5a2662b32b9e86c992165b9b8ca3509dd51e0220
TanYongAN <33213065+TanYongAn@users.noreply.github.com>
2018-10-26 20:04:57 +0800

    ...

--- SDC_FYP/SDC_FYP/www/lib/common.js
+++ SDC_FYP/SDC_FYP/www/lib/common.js
@@ -34,3 +34,19 @@ var hidePopover = function () {
         .getElementById('popover')
         .hide();
 };
+
+//Dont delete this is for sidenav to function properly
+
+window.fn = {};
+
+window.fn.open = function () {
+    var menu = document.getElementById('menu');
+    menu.open();
+};
+
+window.fn.load = function (page) {
+    var content = document.getElementById('content');
+    var menu = document.getElementById('menu');
+    content.load(page)
+        .then(menu.close.bind(menu));
+};
