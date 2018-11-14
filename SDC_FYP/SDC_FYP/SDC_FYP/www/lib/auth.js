34cd7fc10fb354dfd12404cdda07f2a9384566d1
WizardXD <royston123456@gmail.com>
2018-11-14 21:44:16 +0800

    Revert "all working le"
    This reverts commit a32a90bd50af405815cb88f6231877097ffde397.

--- SDC_FYP/SDC_FYP/SDC_FYP/www/lib/auth.js
+++ SDC_FYP/SDC_FYP/SDC_FYP/www/lib/auth.js
@@ -127,10 +127,14 @@
         } else if (data.result == '0') {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             ons.notification.alert('Username or Password is incorrect. Please try again',{
                 title: 'Not Found'
             });
 
+=======
+            alert("not working yo"); //Change this later when i reach home and have time :)
+>>>>>>> parent of a32a90b... all working le
 =======
             alert("not working yo"); //Change this later when i reach home and have time :)
 >>>>>>> parent of a32a90b... all working le
