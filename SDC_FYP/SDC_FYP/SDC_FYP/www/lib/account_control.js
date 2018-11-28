ff08d318b74cef1bf99e08456001e4d73a20965c
JM0810 <43663692+JM0810@users.noreply.github.com>
2018-11-28 18:52:38 +0800

    123

--- SDC_FYP/SDC_FYP/SDC_FYP/www/lib/account_control.js
+++ SDC_FYP/SDC_FYP/SDC_FYP/www/lib/account_control.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
-
+    alert("hi");
+    traildetails();
 
 });
 
---
+++ SDC_FYP/SDC_FYP/SDC_FYP/www/lib/trailmanagement.js
@@ -0,0 +1,43 @@
+﻿
+
+$(document).ready(function () {
+    traildetails();
+
+});
+
+//function to display the details of the artefact, such as image and background information
+function traildetails() {
+    var url = serverURL() + "/trailmanagement.php";
+
+    var JSONObject = {
+    };
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        data: JSONObject,
+        dataType: 'json',
+        contentType: "application/json; charset=utf-8",
+        success: function (arr) {
+            ShowTrailDetails(arr);
+        },
+        error: function () {
+            alert("Error");         //Change to validation message 
+        }
+    });
+}
+
+//function to call if php call is successful
+function ShowTrailDetails(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var t;
+        t = "<ons-card>" +
+            "Trail Name: " + arr[i].trailname + "</br>" +
+            "Trail Location: " + arr[i].traillocation + "</br>" +
+            "Duration: " + arr[i].duration + " hours" + "</br>" +
+            "<center> <ons-button modifier='quiet' id='btnTrail" + arr[i].trailid + "'>" + "Edit </ons-button> </center>";
+            "</ons-card>";
+
+        $("#trail").append(t);
+    }
+}
