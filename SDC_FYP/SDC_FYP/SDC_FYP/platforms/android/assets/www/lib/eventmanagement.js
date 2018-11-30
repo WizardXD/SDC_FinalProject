c24f12023403ae68dbeacfdb9991a9de4dfad8d8
Tabitha29 <43605963+Tabitha29@users.noreply.github.com>
2018-12-01 00:14:16 +0800

    Tabitha First Commit to MobileMaster

---
+++ SDC_FYP/SDC_FYP/SDC_FYP/platforms/android/assets/www/lib/eventmanagement.js
@@ -0,0 +1,50 @@
+﻿(function () {
+
+    $(document).ready(function () {
+        getArtefact();
+    });
+
+    //function to retrieve the details of the artefact, such as artefact id (based on the accesscode)
+    function getArtefact() {
+        var url = serverURL() + "/eventview.php";
+        var userid = localStorage.getItem("userid");
+        var JSONObject = {
+        };
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: JSONObject,
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: function (arr) {
+                _showEventResult(arr);
+            },
+            error: function () {
+                alert("Wrong");       //Change to Validation Message
+            }
+        });
+    }
+
+
+    function _showEventResult(arr) {
+        //if i is less than the total  number of artefacts, increment by 1
+        for (i = 0; i < arr.length; i++) {
+            var t;
+
+            
+            // creating button with id similiar to the artefact id
+            t = "<ons-card>" +
+                "<p> Trail Name: " + "<br/>" + "</p>" + 
+                "<p> Date: " + arr[i].date + "<br/>" + "</p>" +
+                "Facilitator-In-Charge: " + "<br/>" + "</p>" +
+                "<ons-button id='btnEdit" + arr[i].eventid + "'>" + "Edit " + "</ons-button>" +
+                "&nbsp; &nbsp; &nbsp;" +
+                "<ons-button id='btnDelete" + arr[i].eventid + "'>" + "Delete " + " </ons-button>" +
+                "</ons - card>";
+            $("#event").append(t);
+
+        }
+    }
+})();
+
