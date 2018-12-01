63cc294090c6c024c4c6b48c8ef257f8789c8d25
TanYongAN <33213065+TanYongAn@users.noreply.github.com>
2018-12-01 17:01:09 +0800

    ...

--- TrailCovery_MobileBuild/www/js/waitingroom.js
+++ TrailCovery_MobileBuild/www/js/waitingroom.js
@@ -2,11 +2,13 @@
 
     $(document).ready(function () {
 	getEventStatus();
+	getEventDetails();
 	getGroupsInWR();
 	getAdminsInWR();
      });
 	 
-	 
+	 // check the status of the game... if status = ONG, relocate all users to the game page
+	 // the status of the event will only change from YTB to ONG after the admin press on the start game button
 	 function getEventStatus() {
 		 var url = serverURL() + "/wrgame.php";
 			var JSONObject = {
@@ -37,7 +39,44 @@
 		}
 	 }
 
-    //function to retrieve the details of the artefact, such as artefact id (based on the accesscode)
+	 
+	 // display the details of events
+	 function getEventDetails() {
+		var url = serverURL() + "/wrdetails.php";
+			var JSONObject = {
+				"accesscode": localStorage.getItem("accesscode")
+			};
+
+			$.ajax({
+				url: url,
+				type: 'GET',
+				data: JSONObject,
+				dataType: 'json',
+				contentType: "application/json; charset=utf-8",
+				success: function (arr) {
+					_showEventDetailsResult(arr);
+				},
+				error: function () {
+					alert("Wrong");       //Change to Validation Message
+				}
+			});
+	 }
+	 
+	 function _showEventDetailsResult(arr) {
+			 for (i = 0; i < arr.length; i++) {
+				var t;
+				t = 
+				"<ons-card>" + 
+				"<b> Trail Name:  </b>" +  arr[i].trailname + 
+				"<br/>" + "<b>Location: </b>" + arr[i].traillocation + 
+				"<br/>" + "<b> Duration: </b>" + arr[i].duration + " Hours" +  
+				"</ons-card>";
+				$("#wrdetails").append(t);	
+		}
+	 }
+	 
+	 
+    //function to display groups that has entered the x accesscode
     function getGroupsInWR() {
 		var url = serverURL() + "/wrstudents.php";
         var JSONObject = {
@@ -64,11 +103,12 @@
             var t;
             t = 
 			"<ons-card>" + "<img src='" + serverURL() + "/images/avatars/" + arr[i].image + "' width=17%' height='17%'>" + arr[i].groupname + "</ons-card>";
-            $("#groups").append(t);
+            $("#wrgroups").append(t);
 
         }
 	}
 	
+	//display the admin which is incharge of the event --> defined when creating new event.
 	function getAdminsInWR() {
 		var url = serverURL() + "/wradmins.php";
         var JSONObject = {
@@ -91,15 +131,12 @@
     }
 	
 	function _showAdminsInWRResult(arr) {
-	 for (i = 0; i < arr.length; i++) {
-			var t;
-            t = 
-			"<ons-card>" + "<img src='" + serverURL() + "/images/admins/" + arr[i].adminimg + "' width=17%' height='17%'>" + arr[i].adminname + "</ons-card>";
-            $("#admins").append(t);
-			
-		
-	}
-	
+		 for (i = 0; i < arr.length; i++) {
+				var t;
+				t = 
+				"<ons-card>" + "<img src='" + serverURL() + "/images/admins/" + arr[i].adminimg + "' width=17%' height='17%'>" + arr[i].adminname + "</ons-card>";
+				$("#wradmins").append(t);	
+		}
 	}
 	
 })();
