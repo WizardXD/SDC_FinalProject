f2eaf7ac16ca4e4fe10112c52a4815c02ada4025
Tabitha29 <43605963+Tabitha29@users.noreply.github.com>
2018-12-01 20:55:38 +0800

    user management

--- TrailCovery_MobileBuild/platforms/browser/www/js/account_control.js
+++ TrailCovery_MobileBuild/platforms/browser/www/js/account_control.js
@@ -2,6 +2,7 @@
 	
     // For displaying the user details
     var role;
+    var username;
     // Showing the standard header buttons and icons
     $(document).ready(function () {
 
@@ -27,7 +28,6 @@
                 dataType: 'json',
                 contentType: "application/json; charset=utf-8",
                 success: function (arr) {
-					alert("hi");
                     _showRolesResult(arr);
                 },
                 error: function () {
@@ -49,13 +49,17 @@
             }
             t.clear();
 
+
 				for (var i = 0; i < arr.length; i++) {
 					t.row.add([
+                        arr[i].username,
 						arr[i].email,
 						arr[i].school,
 						arr[i].phone,
 						"<a href='#' class='ui-btn' id='btn" + arr[i].username + "'>View</a>"
-					]).draw(false);
+                    ]).draw(false);
+                    
+
 
 					$("#btn" + arr[i].username).bind("click", { id: arr[i].username },
 						function (event) {
@@ -76,3 +80,94 @@
             }
         
 })();
+
+// (function () {
+
+//     $(document).ready(function () {
+//         getUserDetails();
+
+//         $("#btnAddUser").bind("click", function () {
+//                 addUser();
+//         });
+
+//     });
+
+//     //retrieve the details of events
+//     function getUserDetails() {
+//         var url = serverURL() + "/getusers.php";
+//         var JSONObject = {
+//         };
+
+//         $.ajax({
+//             url: url,
+//             type: 'GET',
+//             data: JSONObject,
+//             dataType: 'json',
+//             contentType: "application/json; charset=utf-8",
+//             success: function (arr) {
+//                 _getUserResult(arr);
+//             },
+//             error: function () {
+//                 validationMsg();
+//             }
+//         });
+//     }
+
+//     function _getUserResult(arr) {
+//         for (i = 0; i < arr.length; i++) {
+//             var t;
+//             t = "<ons-card>" +
+//                 "Username: " + arr[i].username + "</br>" +
+//                 "Email: " + arr[i].email + "</br>" +
+//                 "School: " + arr[i].school + "</br>" +
+//                 "Phone: " + arr[i].phone + "</br>" +
+
+//                 "</ons-card>";
+//             $("#User").append(t);
+
+//         }
+//     }
+
+//     //insert user into database
+//     function addUser() {
+//         username = $("#username").val();
+//         password = $("#password").val();
+//         email = $("#email").val();
+//         school = $("#school").val();
+//         phone = $("#phone").val();
+//         role = $("#role").val();
+
+//         url = serverURL() + "/addusers.php"; 
+       
+//         var JSONObject = {
+//             "username": username,
+//             "password": password,
+//             "email": email,
+//             "school": school,
+//             "phone": phone,
+//             "role": role
+//         };
+
+//         $.ajax({
+//             url: url,
+//             type: 'GET',
+//             data: JSONObject,
+//             dataType: 'json',
+//             contentType: "application/json; charset=utf-8",
+//             success: function (arr) {
+//                 _getAddUserResult(arr);
+//             }, error: function () {
+//                 validationMsg();
+//             }
+//         });
+//     }
+
+//     function _getAddUserResult(arr) {
+//         if (arr[0].result === 1) {
+//             alert("User has been created successfully");
+//             window.location = "facilitator.html";
+//         } else if (arr[0].result === 0) {
+//             alert("Please fill up all fields.");
+//         }
+//     }
+// })(); 
