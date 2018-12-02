b5d91455a29db565ff272755f3e6e14fd5c1da98
Tabitha29 <43605963+Tabitha29@users.noreply.github.com>
2018-12-02 23:46:44 +0800

    user management - popup box + add user

--- TrailCovery_MobileBuild/platforms/browser/www/js/account_control.js
+++ TrailCovery_MobileBuild/platforms/browser/www/js/account_control.js
@@ -1,173 +1,215 @@
-(function () {
+// (function () {
 	
-    // For displaying the user details
-    var role;
-    var username;
-    // Showing the standard header buttons and icons
-    $(document).ready(function () {
+//     // For displaying the user details
+//     var role;
+//     var username;
+//     // Showing the standard header buttons and icons
+//     $(document).ready(function () {
 
-	$("#viewRoles").bind("click", function () {
-                showUsers();
-		});
+// 	$("#viewRoles").bind("click", function () {
+//                 showUsers();
+// 		});
 		
-     });
+//      });
 
-        function showUsers() {
+//         function showUsers() {
 
-			var role = $("#selectroles").val();
+// 			var role = $("#selectroles").val();
 			
-            var url = serverURL() + "/getusers.php";
-            var JSONObject = {
-				"role": role
-            };
+//             var url = serverURL() + "/getusers.php";
+//             var JSONObject = {
+// 				"role": role
+//             };
 
-            $.ajax({
-                url: url,
-                type: 'GET',
-                data: JSONObject,
-                dataType: 'json',
-                contentType: "application/json; charset=utf-8",
-                success: function (arr) {
-                    _showRolesResult(arr);
-                },
-                error: function () {
-                    validationMsg();
-                }
-            });
-        }
+//             $.ajax({
+//                 url: url,
+//                 type: 'GET',
+//                 data: JSONObject,
+//                 dataType: 'json',
+//                 contentType: "application/json; charset=utf-8",
+//                 success: function (arr) {
+//                     _showRolesResult(arr);
+//                 },
+//                 error: function () {
+//                     validationMsg();
+//                 }
+//             });
+//         }
 
-        function _showRolesResult(arr) {
-            var t;
-            if ($.fn.dataTable.isDataTable('#showUsers')) {
-                t = $('#showUsers').DataTable();
-            }
-            else {
-                t = $('#showUsers').DataTable({
-                    "searching": false,
-                    "lengthChange": false
-                });
-            }
-            t.clear();
+//         function _showRolesResult(arr) {
+//             var t;
+//             if ($.fn.dataTable.isDataTable('#showUsers')) {
+//                 t = $('#showUsers').DataTable();
+//                 responsive: true
+//             }
+//             else {
+//                 t = $('#showUsers').DataTable({
+//                     "searching": false,
+//                     "lengthChange": false
+//                 });
+//             }
+//             t.clear();
 
 
-				for (var i = 0; i < arr.length; i++) {
-					t.row.add([
-                        arr[i].username,
-						arr[i].email,
-						arr[i].school,
-						arr[i].phone,
-						"<a href='#' class='ui-btn' id='btn" + arr[i].username + "'>View</a>"
-                    ]).draw(false);
+// 				for (var i = 0; i < arr.length; i++) {
+// 					t.row.add([
+//                         arr[i].username,
+// 						arr[i].email,
+// 						arr[i].school,
+// 						arr[i].phone,
+// 						"<a href='#' class='ui-btn' id='btn" + arr[i].username + "'>View</a>"
+//                     ]).draw(false);
                     
 
 
-					$("#btn" + arr[i].username).bind("click", { id: arr[i].username },
-						function (event) {
-							var data = event.data;
-							userresult(data.id);
-						});
+// 					$("#" + arr[i].username).bind("click", { id: arr[i].username },
+// 						function (event) {
+// 							var data = event.data;
+// 							userresult(data.id);
+// 						});
 
-					$("#showUsers").show();
-				}
-		}
+// 					$("#showUsers").show();
+// 				}
+// 		}
 
-            function userresult(username) {
-                window.location = "getaccountbyusername.html?username=" + username;
-                localStorage.setItem("username", username);
-                localStorage.setItem("email", email);
-                localStorage.setItem("school", school);
-                localStorage.setItem("phone", phone);
-            }
+//             function userresult(username) {
+//                 window.location = "getaccountbyusername.html?username=" + username; 
+//             }
         
-})();
+// })();
 
-// (function () {
+    
+      
+ 
 
-//     $(document).ready(function () {
-//         getUserDetails();
 
-//         $("#btnAddUser").bind("click", function () {
-//                 addUser();
-//         });
+(function () {
 
-//     });
+    $(document).ready(function () {
+        
+        $("#viewRoles").bind("click", function () {
+            showUsers();
+        });
+    
 
-//     //retrieve the details of events
-//     function getUserDetails() {
-//         var url = serverURL() + "/getusers.php";
-//         var JSONObject = {
-//         };
+        $("#btnAddUser").bind("click", function () {
+                addUser();
+        });
 
-//         $.ajax({
-//             url: url,
-//             type: 'GET',
-//             data: JSONObject,
-//             dataType: 'json',
-//             contentType: "application/json; charset=utf-8",
-//             success: function (arr) {
-//                 _getUserResult(arr);
-//             },
-//             error: function () {
-//                 validationMsg();
-//             }
-//         });
-//     }
+        $("#addUserDialog").bind("click", function(){
+            addUserDialog();
+        });
 
-//     function _getUserResult(arr) {
-//         for (i = 0; i < arr.length; i++) {
-//             var t;
-//             t = "<ons-card>" +
-//                 "Username: " + arr[i].username + "</br>" +
-//                 "Email: " + arr[i].email + "</br>" +
-//                 "School: " + arr[i].school + "</br>" +
-//                 "Phone: " + arr[i].phone + "</br>" +
+        $("#hideDialog").bind("click", function(){
+            hideDialog();
+        });
+    });
+    
 
-//                 "</ons-card>";
-//             $("#User").append(t);
 
-//         }
-//     }
+    function showUsers() {
 
-//     //insert user into database
-//     function addUser() {
-//         username = $("#username").val();
-//         password = $("#password").val();
-//         email = $("#email").val();
-//         school = $("#school").val();
-//         phone = $("#phone").val();
-//         role = $("#role").val();
+        			var role = $("#selectroles").val();
+                    
+                    var url = serverURL() + "/getusers.php";
+                    var JSONObject = {
+        				"role": role
+                    };
+        
+                    $.ajax({
+                        url: url,
+                        type: 'GET',
+                        data: JSONObject,
+                        dataType: 'json',
+                        contentType: "application/json; charset=utf-8",
+                        success: function (arr) {
+                            _getUserResult(arr);
+                        },
+                        error: function () {
+                            validationMsg();
+                        }
+                    });
+                }
 
-//         url = serverURL() + "/addusers.php"; 
+    function _getUserResult(arr) {
+        var t;
+
+        for (i = 0; i < arr.length; i++) {
+            
+            t = "<ons-card>" +
+                "Username: " + arr[i].username + "</br>" +
+                "Email: " + arr[i].email + "</br>" +
+                "School: " + arr[i].school + "</br>" +
+                "Phone: " + arr[i].phone + "</br>" +
+                "</ons-card>";
+                
+            $("#User").append(t);
+            
+        }
+
+    }
+
+
+
+    var addUserDialog = function() {
+        var dialog = document.getElementById('user-dialog');
+      
+        if (dialog) {
+          dialog.show();
+        } else {
+          ons.createElement('account_management.html', { append: true })
+            .then(function(dialog) {
+              dialog.show();
+            });
+        }
+      };
+
+    //insert user into database
+    function addUser() {
+        username = $("#username").val();
+        password = $("#password").val();
+        email = $("#email").val();
+        school = $("#school").val();
+        phone = $("#phone").val();
+        role = $("#role").val();
+
+        url = serverURL() + "/adduser.php"; 
        
-//         var JSONObject = {
-//             "username": username,
-//             "password": password,
-//             "email": email,
-//             "school": school,
-//             "phone": phone,
-//             "role": role
-//         };
+        var JSONObject = {
+            "username": username,
+            "password": password,
+            "email": email,
+            "school": school,
+            "phone": phone,
+            "role": role
+        };
 
-//         $.ajax({
-//             url: url,
-//             type: 'GET',
-//             data: JSONObject,
-//             dataType: 'json',
-//             contentType: "application/json; charset=utf-8",
-//             success: function (arr) {
-//                 _getAddUserResult(arr);
-//             }, error: function () {
-//                 validationMsg();
-//             }
-//         });
-//     }
+        $.ajax({
+            url: url,
+            type: 'GET',
+            data: JSONObject,
+            dataType: 'json',
+            contentType: "application/json; charset=utf-8",
+            success: function (arr) {
+                _getAddUserResult(arr);
+            }, error: function () {
+                validationMsg();
+            }
+        });
+    }
 
-//     function _getAddUserResult(arr) {
-//         if (arr[0].result === 1) {
-//             alert("User has been created successfully");
-//             window.location = "facilitator.html";
-//         } else if (arr[0].result === 0) {
-//             alert("Please fill up all fields.");
-//         }
-//     }
-// })(); 
+    function _getAddUserResult(arr) {
+        if (arr[0].result === 1) {
+            alert("User has been created successfully");
+            window.location = "facilitator.html";
+        } else if (arr[0].result === 0) {
+            alert("Please fill up all fields.");
+        }
+    }
+    
+    var hideDialog = function() {
+        document
+          .getElementById('user-dialog')
+          .hide();
+      };
+}());
