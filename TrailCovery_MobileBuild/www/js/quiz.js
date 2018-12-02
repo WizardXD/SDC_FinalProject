364d44440a48fbb836414caf2addaece0108eb81
TanYongAN <33213065+TanYongAn@users.noreply.github.com>
2018-12-02 22:22:28 +0800

    Redesign card on quiz and game

--- TrailCovery_MobileBuild/www/js/quiz.js
+++ TrailCovery_MobileBuild/www/js/quiz.js
@@ -35,8 +35,8 @@
     //function to call if php call is successful
     function showArtefactDetails(arr) {
         for (var i = 0; i < arr.length; i++) {
-            $("#frontartefactimage").html("<img src='" + serverURL() + "/images/artefacts/" + arr[i].image + "' width=112%' height='210px' border='2'>");
-            $("#backartefactimage").html("<img src='" + serverURL() + "/images/artefacts/" + arr[i].image + "' width='112%' height='210px' border='2'>");
+            $("#frontartefactimage").html("<img src='" + serverURL() + "/images/artefacts/" + arr[i].image + "' width='90%' border='2'>");
+            $("#backartefactimage").html("<img src='" + serverURL() + "/images/artefacts/" + arr[i].image + "' width='90%' border='2'>");
             $("#artefactbackgroundinfo").html("<p align='justify'>" + arr[i].backgroundinfo + "</p>");
         }
     }
@@ -70,7 +70,7 @@
     // function to execute if php call is successful
     function _getAnswersResults(arr) {
         if (arr[0].result.trim() !== "0") {    //if result is not equal to "0" --> at least a row of data is found in the database with the given answer and the artefact id
-            validationMsgs("Correct Answer", "Information", "OK");  //theres a slight error --> page relocated without even clicking on the valiatonmsg
+            alert("Correct Answer");  //theres a slight error --> page relocated without even clicking on the valiatonmsg
             window.location.href = 'game.html';
         } else {                               //else --> result = 0 --> no data was found with the given answer (wrong inputation of answer)
             validationMsgs("Wrong Answer","Information", "OK");
