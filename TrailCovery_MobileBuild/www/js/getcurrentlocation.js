8b082d34963af96bdfbf1c1810e458a265089dba
WizardXD <royston123456@gmail.com>
2018-12-03 11:45:47 +0800

    updated the UI for Map

--- TrailCovery_MobileBuild/www/js/getcurrentlocation.js
+++ TrailCovery_MobileBuild/www/js/getcurrentlocation.js
@@ -1,24 +1,29 @@
-    document.addEventListener("deviceready", onDeviceReady, false);
-    function onDeviceReady() {
-        console.log("navigator.geolocation works well");
-    }
 var longitude;
 var latitude;
- // onSuccess Callback
-    //   This method accepts a `Position` object, which contains
-    //   the current GPS coordinates
-    //
-    function onSuccess(position) {
+var username = localStorage.getItem('username');    
+
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+} 
+
+function onSuccess(position) {
      
     latitude= position.coords.latitude;
 	longitude= position.coords.longitude;
-	
-			var url = 'https://mp18.projectsbit.org/sdc/locationupdate.php'; //have an issue 
+
+
+
+    var url = 'https://mp18.projectsbit.org/sdc/locationupdate.php'; //have an issue 
 
         //JSONObject
         var JSONObjectMAP = {
             "longitude": longitude, //Key:value
-            "latitude": latitude //Key:value
+            "latitude": latitude, //Key:value
+            "username" : username
+
         };
 
         //ajax to call
@@ -42,12 +47,11 @@ var latitude;
 
     // onError Callback receives a PositionError object
     //
-    function onError(error) {
+function onError(error) {
         alert('code: '    + error.code    + '\n' +
               'message: ' + error.message + '\n');
-    }
+}
 
     // Options: throw an error if no update is received every 30 seconds.
     //
-    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
-	
+var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
