c9280fc9a90ec9ac3727f0e01580db2c34a02373
TanYongAN <33213065+TanYongAn@users.noreply.github.com>
2019-01-22 21:40:08 +0800

    asd

--- TrailCovery_MobileBuild/platforms/browser/www/js/game.js
+++ TrailCovery_MobileBuild/platforms/browser/www/js/game.js
@@ -1,8 +1,12 @@
 (function () {
 
     $(document).ready(function () {
-		getTrailTime();
+        
+        getTrailTime();
         getArtefact();
+            var x = setInterval(function() { // Update the count down every 1 second (1 second is defined by 1000, 2 seconds = 2000, ....)
+            disableArtefactBtn();
+         }, 1000);
     });
 
     //function to retrieve the details of the artefact, such as artefact id (based on the accesscode)
@@ -32,9 +36,9 @@
         for (i = 0; i < arr.length; i++) {
 			$("#trailimage").html("<img src='" + serverURL() + "/images/trails/" + arr[i].trailimg + "' width='90%' length='20%' border='2'>");
             var t;
-            y = "<p><ons-button id='btn" + arr[i].artefactid + "' style='width:45%'>" + "Artefact " + (i+1) + " </ons-button><p/>";
+            y = "<p><ons-button id='btnartefact" + arr[i].artefactid + "' style='width:45%'>" + "Artefact " + (i+1) + " </ons-button><p/>";
             $("#artefactbtn").append(y);
-            $("#btn" + arr[i].artefactid).bind("click", { 
+            $("#btnartefact" + arr[i].artefactid).bind("click", { 
                 id: arr[i].artefactid
              }, function (event) {
                 var data = event.data;
@@ -49,70 +53,180 @@
         window.location = "quiz.html?artefactid=" + artefactid;
     }
 	
-// function to retrieve the start time of the event
-function getTrailTime() {
-	var url = serverURL() + "/countdown.php";
-        var JSONObject = {
-            "accesscode": localStorage.getItem("accesscode")
-        };
+    // function to retrieve the start time of the event
+    function getTrailTime() {
+        var url = serverURL() + "/countdown.php";
+            var JSONObject = {
+                "accesscode": localStorage.getItem("accesscode")
+            };
 
-        $.ajax({
-            url: url,
-            type: 'GET',
-            data: JSONObject,
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
-            success: function (arr) {
-                _showgetTrailTime(arr);	
-            },
-            error: function () {
-                alert("Wrong");       //Change to Validation Message
-            }
-    });
-}
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: JSONObject,
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (arr) {
+                    _showgetTrailTime(arr);	
+                },
+                error: function () {
+                    alert("Wrong");       //Change to Validation Message
+                }
+        });
+    }
 
-function _showgetTrailTime(arr) {
-	for (var i = 0; i < arr.length; i++) {
+    function _showgetTrailTime(arr) {
+        for (var i = 0; i < arr.length; i++) {
 
-        var fulldate = new Date(arr[i].date);
-        var day = fulldate.getDate();
-        var months = (fulldate.getMonth())+1;
-        var years = fulldate.getFullYear();
-		
-        var time = arr[i].starttime; //e.g 16:00:00 (real value is retrieved from the database)
-		var hours = Number(time.substr(0,2)); //retrieve the 0-1 value from starttime (hours) - 16
-		var minutes = Number(time.substr (3,2)); //retrieve the 3-4 value from starttime (minutes) - 00
-		var seconds = Number(time.substr (6,2)); //retrive the 6-7 value from starttime (seconds) - 00
-		
-		var duration = Number(arr[i].duration); //duration of the game is retrieved from the database
-		
-		var endhour = hours + duration; //end time is equal to the addition of start time with duration (e.g. Start Time = 16 Hours, Duration = 2 Hours, End Time = 18 Hours)
-        var enddatetime = new Date (years, months-1, day, endhour, minutes, seconds);  
-		
-        var endTime = new Date(enddatetime).getTime(); // set variable countDownDate as the end time
-		
-		var x = setInterval(function() { // Update the count down every 1 second (1 second is defined by 1000, 2 seconds = 2000, ....)
-
-			var now = new Date().getTime(); // set variable now as current time
-			
-			var distance = endTime - now; // find the differences between the end date and the current time
-			
-			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Time calculations for hours
-			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); // Time calculations for minutes
-			var seconds = Math.floor((distance % (1000 * 60)) / 1000); // Time calculations seconds
+            var fulldate = new Date(arr[i].date);
+            var day = fulldate.getDate();
+            var months = (fulldate.getMonth())+1;
+            var years = fulldate.getFullYear();
             
-                                                                    // __h __m __s to the game end.
-			document.getElementById("countdowntimer").innerHTML = + hours + "h " + minutes + "m " + seconds + "s " + "to the game end."; // push to div with id equals "countdowntimer" set in html
-			
-			// if time is = 0 (0h:0M:0S), -insert an action that will happen-
-			if (distance < 0) {
-				clearInterval(x);
-				document.getElementById("countdowntimer").innerHTML = "Game Ended";
-			}
-		}, 1000); //each 1000 = 1 second, refresh at 1 second interval
-		
-	}
-}
+            var time = arr[i].starttime; //e.g 16:00:00 (real value is retrieved from the database)
+            var hours = Number(time.substr(0,2)); //retrieve the 0-1 value from starttime (hours) - 16
+            var minutes = Number(time.substr (3,2)); //retrieve the 3-4 value from starttime (minutes) - 00
+            var seconds = Number(time.substr (6,2)); //retrive the 6-7 value from starttime (seconds) - 00
+            
+            var duration = Number(arr[i].duration); //duration of the game is retrieved from the database
+            
+            var endhour = hours + duration; //end time is equal to the addition of start time with duration (e.g. Start Time = 16 Hours, Duration = 2 Hours, End Time = 18 Hours)
+            var enddatetime = new Date (years, months-1, day, endhour, minutes, seconds);  
+            
+            var endTime = new Date(enddatetime).getTime(); // set variable countDownDate as the end time
+            
+            var x = setInterval(function() { // Update the count down every 1 second (1 second is defined by 1000, 2 seconds = 2000, ....)
+
+                var now = new Date().getTime(); // set variable now as current time
+                
+                var distance = endTime - now; // find the differences between the end date and the current time
+                
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); // Time calculations for hours
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)); // Time calculations for minutes
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000); // Time calculations seconds
+                
+                                                                        // __h __m __s to the game end.
+                document.getElementById("countdowntimer").innerHTML = + hours + "h " + minutes + "m " + seconds + "s " + "to the game end."; // push to div with id equals "countdowntimer" set in html
+                
+                // if time is = 0 (0h:0M:0S), -insert an action that will happen-
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.getElementById("countdowntimer").innerHTML = "Game Ended";
+                }
+            }, 1000); //each 1000 = 1 second, refresh at 1 second interval
+            
+        }
+    }
+
+
+    function disableArtefactBtn() {
+        var url = serverURL() + "/disabebtns.php";
+            var JSONObject = {
+                "accesscode": localStorage.getItem("accesscode"),
+                "username": localStorage.getItem("username"),
+            };
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: JSONObject,
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (arr) {
+                    _disableArtefactBtnResult(arr);	
+                },
+                error: function () {
+                    alert("Wrong");       //Change to Validation Message
+                }
+        });
+    }
+
+    function _disableArtefactBtnResult(arr) {
+        for (i = 0; i < arr.length; i++) {
+            var artefacta = arr[i].artefacta;
+            var artefactb = arr[i].artefactb;
+            var artefactc = arr[i].artefactc;
+            var artefactd = arr[i].artefactd;
+            var artefacte = arr[i].artefacte;
+            var artefactf = arr[i].artefactf;
+
+            if (artefacta != '') {
+                document.getElementById('btnartefact1').disabled = true;
+                /*document.getElementById('btnartefact7').disabled = true;
+                document.getElementById('btn13').disabled = true;
+                document.getElementById('btn19').disabled = true;
+                document.getElementById('btn25').disabled = true;
+                document.getElementById('btn31').disabled = true;
+                document.getElementById('btn37').disabled = true;
+                document.getElementById('btn43').disabled = true;
+                document.getElementById('btn49').disabled = true;*/
+            } 
+            
+            if (artefactb != ''){
+                document.getElementById('btnartefact2').disabled = true;
+                /*document.getElementById('btn8').disabled = true;
+                document.getElementById('btn14').disabled = true;
+                document.getElementById('btn20').disabled = true;
+                document.getElementById('btn26').disabled = true;
+                document.getElementById('btn32').disabled = true;
+                document.getElementById('btn38').disabled = true;
+                document.getElementById('btn44').disabled = true;
+                document.getElementById('btn50').disabled = true;*/
+            } 
+            
+            if (artefactc != ''){
+                document.getElementById('btnartefact3').disabled = true;
+                /*document.getElementById('btn9').disabled = true;
+                document.getElementById('btn15').disabled = true;
+                document.getElementById('btn21').disabled = true;
+                document.getElementById('btn27').disabled = true;
+                document.getElementById('btn33').disabled = true;
+                document.getElementById('btn39').disabled = true;
+                document.getElementById('btn45').disabled = true;
+                document.getElementById('btn51').disabled = true;*/
+            }
+            if (artefactd != ''){
+                document.getElementById('btnartefact4').disabled = true;
+                /*document.getElementById('btn10').disabled = true;
+                document.getElementById('btn16').disabled = true;
+                document.getElementById('btn22').disabled = true;
+                document.getElementById('btn28').disabled = true;
+                document.getElementById('btn34').disa   bled = true;
+                document.getElementById('btn40').disabled = true;
+                document.getElementById('btn46').disabled = true;
+                document.getElementById('btn52').disabled = true;*/
+            }
+            
+            if (artefacte != ''){
+                document.getElementById('btnartefact5').disabled = true;
+                /*document.getElementById('btn11').disabled = true;
+                document.getElementById('btn17').disabled = true;
+                document.getElementById('btn23').disabled = true;
+                document.getElementById('btn29').disabled = true;
+                document.getElementById('btn35').disabled = true;
+                document.getElementById('btn41').disabled = true;
+                document.getElementById('btn47').disabled = true;
+                document.getElementById('btn53').disabled = true;*/
+            }
+            
+            if (artefactf != ''){
+                document.getElementById('btnartefact6').disabled = true;
+                /*document.getElementById('btn12').disabled = true;
+                document.getElementById('btn18').disabled = true;
+                document.getElementById('btn24').disabled = true;
+                document.getElementById('btn30').disabled = true;
+                document.getElementById('btn36').disabled = true;
+                document.getElementById('btn42').disabled = true;
+                document.getElementById('btn48').disabled = true;
+                document.getElementById('btn54').disabled = true;*/
+            }
+
+            if (artefacta !="" && artefactb !="" && artefactc !="" && artefactd !="" && artefacte !="" && artefactf !=""){
+                //bonus question
+            }
+
+            }
+        }
 
 })();
 
